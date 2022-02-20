7ff04c1844e086af8f96cf194c177681ce2dadda
CubaLibre267 <97138474+CubaLibre267@users.noreply.github.com>
2022-02-20 17:36:42 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,20 @@
+für stempelkarte: zählervariable
+
+Mehtode AddtoBon ín addArticle einfügen und gleichen Preis übergeben,
+der gerade neu übergeben wurde
+
+für rausfinden wea ist kleinster kaffepreis: Zwischenspeichervariable anlegen und chekchen,
+ist neuer Preus größer oder Kleiner als alter? --> bei kleiner: zwischenspeichernr
+
+
+ <div class="bon-artikel-position">
+    <img src="img/kaffee4.png" class="bon-artikel-bild"></img>
+    <div class="bon-artikel-name">Espresso Macchiato klein </div>
+    <div class="bon-artikel-preis">5.00</div>
+</div>
+
+<li>
+    <img src="img/kaffee4.png" class="bon-artikel-bild"></img>
+    <span>Espresso klein</span>
+    <span class="bon-artikel-preis">5.00</span>
+</li>
--- script.js
+++ script.js
@@ -1,19 +1,91 @@
-let price = 0;
+let newPrice = 0;
+let stempelkarte = 0;
 
 function refreshPage() {
     window.location.reload();
 }
+/*
+window.setInterval(function () {
+    let elem = document.getElementById('artikelliste');
+    elem.scrollTop = elem.scrollHeight;
+}, 50);
+*/
 
-function addArticle(newPrice) {
-    price += newPrice;
-
-    price = numb = Math.round((price + Number.EPSILON) * 100) / 100;
-
-    let payPreis = document.getElementById("pay-preis");
-    payPreis.innerText = price;
+function updateScroll() {
+    var element = document.getElementById("yourDivID");
+    element.scrollTop = element.scrollHeight;
 }
 
-//für stempelkarte: zählervariable
+function bestellung(itemname, itemprice) {
 
-//für rausfinden wea ist kleinster kaffepreis: Zwischenspeichervariable anlegen und chekchen,
-//ist neuer Preus größer oder Kleiner als alter? --> bei kleiner: zwischenspeichernr
+    stempelKarteUpdate();
+
+    addPrice(itemprice);
+
+    const orderlist = document.getElementById('orderlist');
+
+    //LI tag erstellen
+    const orderitem = document.createElement('li');
+
+    //  IMAGE SECTION
+    //img tag erstellen
+    const orderitemimgtag = document.createElement('img');
+    //Klassenname zu img anhängen
+    orderitemimgtag.className = 'bon-artikel-bild';
+    //Src zu img tag hinzufügen 
+    orderitemimgtag.src = 'img/kaffee4.png';
+    //Child an LI anhängen
+    orderitem.appendChild(orderitemimgtag);
+
+    //Span für rote Farbe erstellen
+    const orderitempricespan = document.createElement('span');
+
+    //textnode erstellen mit itemname and itemprice
+    const orderitemname = document.createTextNode(itemname + ' ');
+    const orderitemprice = document.createTextNode(itemprice + ' €');
+
+    //span die klasse für rote farbe geben 
+    orderitempricespan.className = 'bon-artikel-preis';
+
+    //pricetextnode span zu span hinzufügen
+    orderitempricespan.appendChild(orderitemprice);
+
+    //Itemname tag und itemprice an LI anhängen
+    orderitem.appendChild(orderitemname);
+
+    //orderitempricespan an LI anhängen
+    orderitem.appendChild(orderitempricespan);
+
+    //LI an Parent id=orderlist anhängen
+    orderlist.appendChild(orderitem);
+}
+
+function addPrice(price) {
+
+    price += newPrice;
+
+    newPrice = numb = Math.round((price + Number.EPSILON) * 100) / 100;
+
+    let footerPreis = document.getElementById("footer-preis");
+    footerPreis.innerText = newPrice;
+}
+
+function stempelKarteUpdate() {
+    stempelkarte++;
+
+    const sk = document.getElementById('anzahl-stempel');
+    sk.innerText = stempelkarte;
+}
+
+function bezahlung() {
+
+    //Price wieder auf null
+    newPrice = 0.00;
+    let footerPreis = document.getElementById("footer-preis");
+    footerPreis.innerText = '0.00';
+
+    //Bon leeren
+    const orderlist = document.getElementById('orderlist');
+    orderlist.innerText = '';
+}
+
